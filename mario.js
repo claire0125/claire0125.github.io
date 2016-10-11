91fc636e10b74d3c01eacb49e5192766a8dc6c24
claire0125 <claire0125000@gmail.com>
2016-10-11 16:47:03 -0700

    Rename index.html to mario.js

---
+++ mario.js
@@ -0,0 +1,30 @@
+//mario's directio
+var x=200,y=0
+var dirction=1;
+
+
+var cel=0;
+
+functionmovemario
+
+
+
+<head>
+  <title>it's mario </title>
+  <style>
+    mario{
+    background-position:-60px o;
+    left:250 px;
+    posiotion:absolutate
+    transfprm:scalex(1);
+    }
+    </style>
+    <script src="//cdnjs.clouflare.com/ajax/libs/jquare.min.js"></script>
+    <script src"mario.js"></script>
+    </head>
+    
+    <body>
+    <div class="mario"></div>
+    <button class=go">Go</button>
+    <botton class= "action">Look out,Mario</button>
+    </body>
